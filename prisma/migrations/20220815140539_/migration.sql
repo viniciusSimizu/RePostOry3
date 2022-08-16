50d3bac4046c559413153b7a20c503c5bf49df60
viniciussimizu <vinicius.simizu@ekaizen.digital>
2022-08-15 23:18:38 -0300

    feat(docker): configuration

--- prisma/migrations/20220815140539_/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE `github_account` MODIFY `deleted` BIT(1) NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `repository` MODIFY `description` MEDIUMTEXT NULL,
-    MODIFY `deleted` BIT(1) NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `user` MODIFY `deleted` BIT(1) NOT NULL DEFAULT false;
